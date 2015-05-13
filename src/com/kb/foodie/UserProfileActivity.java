704afad84be6710a074387fde5e6c11a49c96797
Himani Ahuja <himani.ahuja@samsung.com>
2015-05-12 17:30:53 -0700

    basic app page

---
+++ src/com/kb/foodie/UserProfileActivity.java
@@ -0,0 +1,13 @@
+package com.kb.foodie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserProfileActivity extends Activity{
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup_dialog);
+	}
+}
