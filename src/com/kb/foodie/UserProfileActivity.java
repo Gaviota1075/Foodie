53c335d6fee296300faee448f78a5fb12ae7eb3c
Himani Ahuja <himani.ahuja@samsung.com>
2015-05-13 17:05:06 -0700

    Initial layout added - service and provider page

--- src/com/kb/foodie/UserProfileActivity.java
+++ src/com/kb/foodie/UserProfileActivity.java
@@ -2,12 +2,21 @@ package com.kb.foodie;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserProfileActivity extends Activity{
 
-	@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startup_dialog);
-	}
+    }
+
+    public void handleServiceProvider(View view){
+        setContentView(R.layout.service_provider_home);
+    }
+
+    public void handleServiceFinder(View view){
+        setContentView(R.layout.service_home);
+    }
 }
